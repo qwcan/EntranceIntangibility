64980f988770bad0ebcfbfe8bc7506a0f805c9e4
qwcan <ieatspamforbreakfasts1@gmail.com>
2026-03-03 22:50:51 -0600

    oops

--- EntranceIntangibility/patch/ShipTeleporterPatch.cs
+++ EntranceIntangibility/patch/ShipTeleporterPatch.cs
@@ -10,13 +10,13 @@ public class ShipTeleporterPatch
     
     [HarmonyPostfix]
     [HarmonyPatch(typeof(ShipTeleporter), "SetPlayerTeleporterId")]
-    private static void PlayerIsTargetablePatch(
+    private static void SetPlayerTeleporterIdPatch(
         ShipTeleporter __instance, PlayerControllerB playerScript, int teleporterId )
     {
         //This *should* only be called when the player is actually teleported
         if (teleporterId == -1)
         {
-            Plugin.Log.LogInfo($"Player {playerScript.playerClientId} entered the facility (ServerRPC), giving them intangibility for {Plugin.Config.IntangibilityDuration.Value} seconds: {DateTimeOffset.Now.ToUnixTimeMilliseconds()}");
+            Plugin.Log.LogInfo($"Player {playerScript.playerClientId} teleported giving them intangibility for {Plugin.Config.IntangibilityDuration.Value} seconds: {DateTimeOffset.Now.ToUnixTimeMilliseconds()}");
             Plugin.Instance.LastEnteredDoor[(int)playerScript.playerClientId] = DateTimeOffset.Now.ToUnixTimeMilliseconds();
         }
     }
